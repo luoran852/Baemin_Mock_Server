ede6a7461dd34047d379385ecd1de591447c0151
luoran852 <hannn0414@naver.com>
2021-08-20 23:53:13 +0900

    add get fast delivery api

--- src/main/java/com/example/demo/src/store/StoreController.java
+++ src/main/java/com/example/demo/src/store/StoreController.java
@@ -3,11 +3,14 @@ package com.example.demo.src.store;
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.store.model.*;
+import com.example.demo.src.user.model.GetUserRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.example.demo.config.BaseResponseStatus.*;
 import static com.example.demo.config.BaseResponseStatus.INVALID_USER_JWT;
@@ -33,7 +36,29 @@ public class StoreController {
         this.jwtService = jwtService;
     }
 
-
+    /**
+     * 우리동네 빠른배달 조회 API
+     * [GET] /users
+     * 회원 번호 및 이메일 검색 조회 API
+     * [GET] /users? Email=
+     * @return BaseResponse<List<GetUserRes>>
+     */
+    //Query String
+    @ResponseBody
+    @GetMapping("/fast-delivery") // (GET) 127.0.0.1:9000/app/users
+    public BaseResponse<List<GetUserRes>> getFastDelivery(@RequestParam(required = false) String Email) {
+        try{
+            if(Email == null){
+                List<GetUserRes> getUsersRes = userProvider.getUsers();
+                return new BaseResponse<>(getUsersRes);
+            }
+            // Get Users
+            List<GetUserRes> getUsersRes = userProvider.getUsersByEmail(Email);
+            return new BaseResponse<>(getUsersRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
 
 
 
